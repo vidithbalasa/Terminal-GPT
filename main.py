8fea41914fea2d05beeea09191d173fa8ce4c6ed
Vidith Balasa <vidithbalasa@Vidiths-MBP.attlocal.net>
2023-05-03 23:51:26 -0700

    Fixed some minor bugs

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+import threading
 import curses
 import string
 import openai
 import textwrap
-from rich.console import Console
-from rich.markdown import Markdown
-from rich.text import Text
+import signal
 
 class GPT:
-    def __init__(self, model: str = 'gpt-3.5-turbo'):
-        self.temperature = 0.7
-        self.max_tokens = 10
+    def __init__(self, model: str = 'gpt-4'):
+        self.temperature = 0.5
+        self.max_tokens = 2000
 
         if model not in {'gpt-3.5-turbo', 'gpt-4', 'gpt-4-0314'}:
             raise ValueError('Please enter a valid openai model')
@@ -44,7 +43,7 @@ class App:
         self.stdscr.timeout(100)
 
         # Create a window for the chat history
-        self.window = curses.newwin(curses.LINES - 2, curses.COLS, 0, 0)
+        self.window = curses.newpad(10000, curses.COLS)
         self.window.scrollok(False)
         self.scroll_position = 0
 
@@ -60,34 +59,30 @@ class App:
         self.show_settings = False
 
         while True:
-            # Get user input
+            signal.signal(signal.SIGWINCH, lambda signum, frame: self.sigwinch_handler(signum, frame, self.stdscr))
+
             self.handle_input()
 
-            rows = ( len(self.user_input) // curses.COLS ) + 1
-            
-            if not self.shown_default:
-                self._update_chat_window()
-                self.shown_default = True
+            self._update_chat_window()
+
             if not self.user_input:
                 self._reset_input()
 
     def handle_input(self):
-        self.stdscr.keypad(True)
         c = self.stdscr.getch()
 
-         # Arrow up
+        # Arrow up
         if c == curses.KEY_UP:
-            self.handle_scroll('up')
+            self._handle_scroll('up')
             return
 
         # Arrow down
         if c == curses.KEY_DOWN:
-            self.handle_scroll('down')
+            self._handle_scroll('down')
             return
 
         # Enter
         if c == ord('\n'):
-            self._reset_input()
             self._update_chat_window()
             self.handle_output()
             self.user_input = ""
@@ -96,7 +91,7 @@ class App:
         # Backspace
         if c == ord('\x7f'):
             self.user_input = self.user_input[:-1]
-        elif c != -1 and (chr(c).isalnum() or chr(c).isspace() or chr(c) in string.punctuation):
+        elif c != -1 and (c in range(65,91) or c in range(97,123) or c in range(48,58) or chr(c).isspace() or chr(c) in string.punctuation):
             self.user_input += chr(c)
  
         wrapped_text = textwrap.wrap('Type here: ' + self.user_input, curses.COLS-1)
@@ -118,6 +113,10 @@ class App:
         self.chat_history.append({'role':'assistant','content':''})
         self._update_chat_window()
 
+        gpt_thread = threading.Thread(target=self._gpt_call)
+        gpt_thread.start()
+
+    def _gpt_call(self):
         combined_output = ''
         for tokens in self.gpt.call(self.chat_history, self.user_input):
             output_delta = tokens['choices'][0]['delta']
@@ -127,37 +126,12 @@ class App:
             self.chat_history[-1]['content'] = combined_output
             self._update_chat_window()
 
-    '''
     def _update_chat_window(self):
+        # Resize the chat window based on the input window size
+        chat_window_height = curses.LINES - self.old_rows - 1
+
         self.window.erase()
         lines_used = 0
-        for i, msg in enumerate(self.chat_history[-(curses.LINES - 3):]):
-            name = str.capitalize(msg['role'])
-            content = msg['content']
-            formatted_str = f'{str.capitalize(name)}: {content}'
-
-            """
-            rendered_msg = self._render_markdown(content)
-            self.window.addstr(i+lines_used, 0, f"{name}: ")
-            for line in rendered_msg:
-                self.window.addstr(i+lines_used, len(name)+2, line.text)
-                lines_used += 1
-            """
-
-            split_text = formatted_str.split('\n')
-            for text_item in split_text:
-                wrapped_text = textwrap.wrap(text_item, curses.COLS)
-                for i, line in enumerate(wrapped_text):
-                    self.window.addstr(i+lines_used, 0, line)
-                lines_used += len(wrapped_text) + 1
-
-        self.window.refresh()
-    '''
-
-    def _update_chat_window(self):
-        self.window.erase()
-        lines_used = 0
-        self.total_lines = sum(len(textwrap.wrap(f"{str.capitalize(msg['role'])}: {msg['content']}", curses.COLS)) for msg in self.chat_history)
 
         for i, msg in enumerate(self.chat_history):
             name = str.capitalize(msg['role'])
@@ -168,21 +142,28 @@ class App:
             for text_item in split_text:
                 wrapped_text = textwrap.wrap(text_item, curses.COLS)
                 for i, line in enumerate(wrapped_text):
-                    if lines_used >= self.scroll_position and lines_used - self.scroll_position < curses.LINES - 2:
-                        self.window.addstr(lines_used - self.scroll_position, 0, line)
+                    self.window.addstr(lines_used, 0, line)
                     lines_used += 1
+            self.window.addstr(lines_used, 0, '-'*curses.COLS)
+            lines_used += 1
 
-        self.window.refresh()
+        #self.window.refresh(self.scroll_position, 0, 0, 0, curses.LINES - 3, curses.COLS)
+        self.window.refresh(self.scroll_position, 0, 0, 0, chat_window_height, curses.COLS)
 
     def _reset_input(self):
         self.input_win.clear()
-        self.input_win.addstr(0, 0, 'Type here: ')
+        self.input_win.resize(1, curses.COLS)
+        self.input_win.mvwin(curses.LINES - 1, 0)
+        self.input_win.addstr(0, 0, f'Type here: {self.user_input}')
         self.input_win.refresh()
 
-    def handle_scroll(self, direction):
+    def _handle_scroll(self, direction):
+        total_lines = self._calculate_total_lines()
+        max_scroll_position = max(0, total_lines - (curses.LINES - 3))
+
         if direction == 'up' and self.scroll_position > 0:
             self.scroll_position -= 1
-        elif direction == 'down' and self.scroll_position < self.total_lines - (curses.LINES - 2):
+        elif direction == 'down' and self.scroll_position < max_scroll_position:
             self.scroll_position += 1
         self._update_chat_window()
 
@@ -191,6 +172,20 @@ class App:
         md = Markdown(markdown_text)
         text = console.render(md)
         return text
+    
+    def _calculate_total_lines(self):
+        total_lines = 0
+        for msg in self.chat_history:
+            name, content = msg['role'], msg['content']
+            split_msg = f"{name}: {content}".split('\n')
+            num_lines = lambda m: len(textwrap.wrap(m, curses.COLS))
+            total_lines += sum([num_lines(x) for x in split_msg])
+        return total_lines
+
+    def sigwinch_handler(self, signum, frame, stdscr):
+        curses.endwin()
+        stdscr.refresh()
+        curses.update_lines_cols()
 
 if __name__ == "__main__":
     curses.wrapper(App)
